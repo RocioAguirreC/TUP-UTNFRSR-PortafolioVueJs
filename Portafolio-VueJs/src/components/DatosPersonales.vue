4277603288d4c9238d0abe53938f6a30d1ad0e18
RocioAguirreC <rocio.aguirre.cerullo@gmail.com>
2024-11-08 23:20:10 -0300

    Nueva carpeta VueJs

---
+++ Portafolio-VueJs/src/components/DatosPersonales.vue
@@ -0,0 +1,83 @@
+<script setup>
+import linkedin from '/src/assets/linkedin_icon.svg';
+import instagram from '/src/assets/instagram_icon.svg';
+import github from '/src/assets/github_icon.svg';
+import cv from '/src/assets/cv_resume_icon.svg';
+
+const title = 'Rocío Aguirre Cerullo';
+const descripcion = 'Student at UTN-FRSR "University Technical Degree in Programming"';
+const residencia = 'Chaco, Argentina';
+const presentacion = 'Hi! My name is Rocío, I am a passionate learner and a curious one, always eager to learn new things and explore the world around me.';
+const redesSociales = [
+  { id: 1, name: 'linkedin', src: linkedin, url: 'https://www.linkedin.com/in/rocio-aguirre-cerullo/' },
+  { id: 2, name: 'Instagram', src: instagram, url: 'https://www.instagram.com/raguirrecerullo/' },
+  { id: 3, name: 'github', src: github, url: 'https://www.github.com/RocioAguirreC' },
+  { id: 4, name: 'curriculum', src: cv, url: '' },
+];
+const telefono = '+54 9 362-4703488';
+</script>
+
+<template>
+  <section class="datos-personales">
+    <div class="card">
+      <h1>{{ title }}</h1>
+      <h2>{{ descripcion }}</h2>
+      <p>{{ presentacion }}</p>
+      <ul class="container-lista">
+        <li v-for="red in redesSociales" :key="red.id">
+          <a :href="red.url"><img class="icon-redsocial" :src="red.src" width="35rem" :alt="red.name"></a>
+        </li>
+      </ul>
+      <h3>☎ My contact information: {{ telefono }}</h3>
+      <h4>{{ residencia }}</h4>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2.5rem;
+}
+
+p {
+  font-size: 1.2rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+}
+
+.card {
+  background-color: rgb(28, 41, 52);
+  border-radius: 10px;
+  padding: 10px;
+  margin: 10px;
+  text-align: center;
+}
+
+.container-lista {
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  text-align: center;
+}
+
+.icon-redsocial {
+  align-items: center;
+  background-color: aliceblue;
+  border-radius: 50%;
+  padding: 2px;
+  margin: 5px;
+  box-shadow: 0 0 5px rgba(95, 124, 205, 0.934);
+}
+
+.icon-redsocial:hover {
+  background-color: rgb(28, 41, 52);
+  box-shadow: 0 0 5px rgba(251, 249, 249, 0.934);
+}
+
+h3 {
+  font-size: 1.2rem;
+  font-weight: 600;
+}
+</style>
---
+++ Portafolio-VueJs/src/components/InteresesComponente.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { ref } from 'vue';
+import FondoLava from './FondoLava.vue';
+//Este es un arreglo con ref para que se pueda reactivar el cambio de los intereses
+const intereses = ref([
+    'Contributor to open-source data projects, focusing on cleaning datasets, visualizing data, and providing insights. Active participant on GitHub, especially with data science repositories.',
+    'Enjoy analyzing public datasets to uncover trends in fields like healthcare, education, or climate. Recently explored a project on air quality impact on public health.',
+    'Passionate about understanding how users interact with applications. Conduct informal UX testing in personal projects to optimize user satisfaction.',
+    'Enthusiast about transforming raw data into compelling visual stories that aid decision-making. Enjoy experimenting with tools like Tableau and Power BI to communicate insights effectively.'
+]);
+</script>
+
+<template>
+    <div class="intereses-contenedor">
+        <FondoLava />
+        <ul class="contenedor-lista">
+            <li class="item" v-for="interes in intereses" :key="interes">
+                {{ interes }}
+            </li>
+        </ul>
+    </div>
+    <!-- <div class="intereses-contenedor">
+        <ul class="contenedor-lista">
+            <li class="item">
+                Desarrollo de Software de Código Abierto: Contribuyo a proyectos en GitHub, colaborando con otros
+                desarrolladores para mejorar herramientas y librerías populares.
+            </li>
+            <li class="item">
+                Deportes al Aire Libre: Disfruto del senderismo y el ciclismo de montaña, actividades que practico
+                regularmente para mantener un equilibrio entre el trabajo y la vida personal.
+            </li>
+            <li class="item">
+                Tecnología de Innovación: Me apasiona explorar nuevas tendencias tecnológicas como la inteligencia
+                artificial y el desarrollo de aplicaciones móviles.
+            </li>
+            <li class="item">
+                Fotografía: Practico la fotografía como un hobby, lo que me ayuda a mantener una perspectiva creativa
+                tanto en mi vida profesional como personal.
+            </li>
+        </ul>
+    </div> -->
+</template>
+
+<style scoped>
+.intereses-contenedor {
+    display: flex;
+    position: relative;
+    margin: 0 auto;
+    max-width: 85%;
+    padding: 1rem;
+}
+
+.contenedor-lista {
+    list-style-type: none;
+    padding: 1rem;
+    margin-bottom: .5rem;
+    color: var(--vt-c-white-soft);
+    font-size: 1.4rem;
+    text-shadow: 3px 3px 6px rgba(0, 0, 0, 1);
+}
+
+.item {
+    margin-bottom: 1rem;
+}
+</style>
